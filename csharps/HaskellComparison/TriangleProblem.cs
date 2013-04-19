42c9ac326173fea1893fb2c6de2ece79e637383c
Wesley McClure <wes.mcclure@gmail.com>
2013-04-18 23:49:55 -0500

    do triangle problem in c#

---
+++ csharps/HaskellComparison/TriangleProblem.cs
@@ -0,0 +1,24 @@
+﻿namespace HaskellComparison
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class TriangleProblem
+    {
+        [Test]
+        public void FindTrianglesWithPerimeter24AndEachSideAnIntegerLessThan10()
+        {
+            // as one line: var results = from a in Enumerable.Range(1, 10) from b in Enumerable.Range(1, 10) from c in Enumerable.Range(1, 10) where (a + b + c == 24) && ((a*a + b*b) == c*c) select new {a, b, c};
+
+            // but more readable IMO
+            var results = from a in Enumerable.Range(1, 10)
+                          from b in Enumerable.Range(1, 10)
+                          from c in Enumerable.Range(1, 10)
+                          where (a + b + c == 24) && ((a*a + b*b) == c*c)
+                          select new {a, b, c};
+
+            results.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
